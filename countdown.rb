857fda21f21acc2a9d219ec2eac8ecccc727d94a
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-10 16:06:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(counter)
+  counter = 0
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter-=1
+  end
+  puts "HAPPY NEW YEAR!"
 end
